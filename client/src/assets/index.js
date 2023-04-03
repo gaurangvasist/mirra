ba71005d92289f194a713c58794bcb90e8e4ebec
gaurangvasist <gaurang123.gv@gmail.com>
2023-04-03 21:51:18 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import miraLogo from './miraLogo.svg';
+
+export {
+  download,
+  logo,
+  miraLogo,
+  preview,
+};
